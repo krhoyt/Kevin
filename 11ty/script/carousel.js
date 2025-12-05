966028ed31ac543e7207b34b4f2d48042ef9bc67
Kevin Hoyt <parkerkrhoyt@gmail.com>
2025-12-05 14:48:28 -0700

    Lighthouse updates
    Mostly in accessibility.

--- 11ty/script/carousel.js
+++ 11ty/script/carousel.js
@@ -121,7 +121,7 @@ export default class HoytCarousel extends HTMLElement {
         }
       </style>
       <section>
-        <button>
+        <button aria-label="Previous" type="button">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
             <path fill="currentColor" d="m10.8 12l3.9 3.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275l-4.6-4.6q-.15-.15-.212-.325T8.425 12t.063-.375t.212-.325l4.6-4.6q.275-.275.7-.275t.7.275t.275.7t-.275.7z"/>
           </svg>      
@@ -129,7 +129,7 @@ export default class HoytCarousel extends HTMLElement {
         <article> 
           <slot></slot>
         </article>
-        <button>
+        <button aria-label="Next" type="button">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
             <path fill="currentColor" d="M12.6 12L8.7 8.1q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7z"/>
           </svg>      
@@ -181,6 +181,7 @@ export default class HoytCarousel extends HTMLElement {
       }
 
       for( let c = 0; c < this.$footer.children.length; c++ ) {
+        this.$footer.children[c].ariaLabel = `View #${c + 1}`;
         this.$footer.children[c].setAttribute( 'data-index', c );
 
         if( c === index ) {
